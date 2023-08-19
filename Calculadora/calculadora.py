4c82e609432e15a9b142edd428d1981e6b31c63e
Paulo Lima <108300157+paulo5150@users.noreply.github.com>
2023-08-19 12:13:00 -0300

    Ajustes

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,19 +1,15 @@
 def calculadora(num1, num2, selecao):
 	if selecao == 1:
-		resultado = num1 + num2
-		return resultado
+		return num1 + num2
 	elif selecao == 2:
-		resultado = num1 - num2
-		return resultado
+		return num1 - num2
 	elif selecao == 3:
-		resultado = num1 * num2
-		return resultado
+		return num1 * num2
 	elif selecao == 4:
 		if num2 == 0:
 			return "Operação Inválida"
-		else:
-			resultado = num1 / num2
-			return resultado
+		else: 
+			return num1 / num2
 	else:
 		return 0
 
